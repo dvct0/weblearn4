89547c30f0251919d8d69f1b79ffd065f1085698
dvct0 <sema.shm2@gmail.com>
2024-04-28 22:01:52 +0500

    First commit

---
+++ components/navbar/navbar.php
@@ -0,0 +1,25 @@
+<?php
+echo '<link rel="stylesheet" href="/navbar.css">
+<div class="navbar">
+<div class="navbar_bgc">
+    <div class="navbuttons">
+        <div class="navbuttons_element">
+            eqwe
+        </div>
+        <div class="navbuttons_element">
+
+        </div>
+        <div class="navbuttons_element">
+
+        </div>
+        <div class="navbuttons_element">
+
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="navbar_search">
+    <input>
+</div>';
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$pageTitle = 'Test page';
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        <?= $pageTitle; ?>
+        <meta charset="utf-8">
+    </title>
+    
+</head>
+
+<body>
+    <?php include ('components/navbar/navbar.php'); ?>
+</body>
+
+</html>
